be4cc9e4584cb711a3ac2d55d61ff13bb32ae438
ruvolpe <ru.volpe@gmail.com>
2024-02-11 18:46:25 +0000

    feat: completed chapter 5

--- Ch05/05_01/start/react-app/src/App.js
+++ Ch05/05_01/start/react-app/src/App.js
@@ -1,21 +1,31 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useState } from "react";
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  return [
+    { value, onChange: (e) => setValue(e.target.value) },
+    () => setValue(initialValue),
+  ];
+}
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
+  const submit = (e) => {
+    e.preventDefault();
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle();
+    resetColor();
+  };
+
   return (
     <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
+      <form onSubmit={submit}>
+        <input {...titleProps} type="text" placeholder="color title" />
+        <input {...colorProps} type="color" />
+        <button>ADD</button>
+      </form>
     </div>
   );
 }
